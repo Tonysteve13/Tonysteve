331dbc557a66dee0230e35c6a61c0b2f1ef01595
Tonysteve13 <ts4665768@gmail.com>
2023-08-26 14:32:11 +0100

    new commit

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -10,11 +10,11 @@ const About = () => {
   return (
     <main className="section container">
       <section className='about'>
-        <h2 className='section__title'>
+        <h2 className='section__title' data-aos="fade-left" data-aos-duration="3000">
           About <span>Me</span>
         </h2>
 
-          <p className="home__description" data-aos="fade-left" data-aos-duration="3000">Having experience in frontend development with languages such as <span>HTML</span> <span>CSS,</span> <span>JS(Es6),</span><span>Boostrap</span>. Also work on projects with frameworks like <span>React.js</span> using technologies such as <span>Redux,</span> <span>Firebase,</span> <span>Material U</span>I and a little <span>Typescript</span>. UI/UX designing using <span>FIGMA,</span> and little knowledge in <span>Graphic designing.</span> I bring a versatile skill set that allows me to create user centered designs that engage users. I am continuously striving to stay ahead on the curve in my pursuit of learning and keep on building my skills. Collaborative and good at team work.</p>
+          <p className="home__description">Having experience in frontend development with languages such as <span>HTML</span> <span>CSS,</span> <span>JS(Es6),</span><span>Boostrap</span>. Also work on projects with frameworks like <span>React.js</span> using technologies such as <span>Redux,</span> <span>Firebase,</span> <span>Material U</span>I and a little <span>Typescript</span>. UI/UX designing using <span>FIGMA,</span> and little knowledge in <span>Graphic designing.</span> I bring a versatile skill set that allows me to create user centered designs that engage users. I am continuously striving to stay ahead on the curve in my pursuit of learning and keep on building my skills. Collaborative and good at team work.</p>
           <p> Let's collaborate and build something great together.!!</p> <br />
           
         <div className="about__container grid">
