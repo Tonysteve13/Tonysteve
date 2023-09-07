95f329d85c3c690a2a6773b7b832dce4e315c0ed
Tonysteve13 <ts4665768@gmail.com>
2023-09-07 15:39:58 +0100

    new commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Profile from "../../assets/home-min.jpg";
 import { Link } from 'react-router-dom';
 import {FaArrowRight} from "react-icons/fa";
@@ -12,11 +11,10 @@ const Home = () => {
       <div className='home__content'>
         <div className='home__data'>
           <h1 className="home__title">
-            <span>I'm Tony Steve. </span>Frontend Developer
+          <span>Hey there.... </span> <br />I'm Tony Steve
           </h1>
 
-          <p className="home__description" data-aos="fade-down" data-aos-duration="3000">Having experience in frontend development with languages such as <span>HTML</span> <span>CSS,</span> <span>JS(Es6),</span><span>Boostrap</span>. Also work on projects with frameworks like <span>React.js</span> using technologies such as <span>Redux,</span> <span>Firebase,</span> <span>Material U</span>I and a little <span>Typescript</span>. UI/UX designing using <span>FIGMA,</span> and little knowledge in <span>graphic design,</span> I bring a versatile skill set that allows me to create user centered designs that engage users. I am continuously striving to stay ahead on the curve in my pursuit of learning and keep on building my skills. Collaborative and good at team work.</p>
-          <p> Let's collaborate and build something great together.!!</p> <br />
+          <p className="home__description" data-aos="fade-down" data-aos-duration="3000">I'm a Front-end Developer, UI/UX and Graphics Designer </p>
           <Link to='/about' className='button'>
               More About Me{' '}
             <span className='button__icon'> 
